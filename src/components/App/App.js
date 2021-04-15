aef93e3893ad205af6c136424325b87d967dccac
Shyamal <sgprajapati24@gmail.com>
2021-04-16 00:49:26 +0530

    Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./App.css"
 import Nav from './../Navbar/Navbar' 
-
+import Main from './../Main/Main'
 class App extends React.Component {
   render() {
     return (
@@ -9,7 +9,7 @@ class App extends React.Component {
         {/* Navbar Section */}
           <Nav/>
           {/* Landing page */}
-
+        <Main/>
         {/* Challenge Section */}
 
         {/* Footer */}
