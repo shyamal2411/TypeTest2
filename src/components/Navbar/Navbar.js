df3cd48c120b94f61767b800b545f309076ae68d
Shyamal <sgprajapati24@gmail.com>
2021-04-15 23:59:36 +0530

    Navbar complete

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import logo from './../../assets/logo.png'
-import "./Navbar.css"
-const Nav =()=>{
-    return (
-        <div className="nav-container">
-            <div className="nav-left">
-                <img className="flash-logo" src={logo} alt="logo" />
-            <p className="flash-logo-text">Test Type</p>
-            </div>
-            {/* Nav left complete */}
-        </div>
-    );
-}
+import React from "react";
+import logo from "./../../assets/logo.png";
+import "./Navbar.css";
+const Nav = () => {
+  return (
+    <div className="nav-container">
+      <div className="nav-left">
+        <img className="flash-logo" src={logo} alt="logo" />
+        <p className="flash-logo-text">Test Type</p>
+      </div>
+      {/* Nav left complete */}
+      <div className="nav-right">
+      <a target="_blank" 
+      className="nav-link"
+      href="https://www.sgprajapati.com"
+      rel="noreferrer"
+      >SG Prajapati</a>
+
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
