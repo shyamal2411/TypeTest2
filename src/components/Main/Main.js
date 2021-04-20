02ba6cb946679170aff9b0a8d2f112544ae73d77
Shyamal Prajapati <sgprajapati24@gmail.com>
2021-04-20 22:43:29 +0530

    typewriter included from sandbox

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,24 +1,27 @@
 import React from "react";
 import "./Main.css";
-import hero from "./../../assets/hero.png"
+import hero from "./../../assets/hero.png";
+import Typewriter from "typewriter-effect";
+
 const Main = () => {
-  return( <div className="main-container">
-    <div data-aos="fade-right" className="main-left">
+  return (
+    <div className="main-container">
+      <div data-aos="fade-right" className="main-left">
         <h1 className="main-header">How fast are you?</h1>
         <div className="typewriter-container">
-            <p>Fast</p>
-            <p>Correct</p>
-            <p>Quick</p>
+          <Typewriter
+            options={{
+              strings: ["Hello", "World"],
+              autoStart: true,
+              loop: true
+            }}
+          />
         </div>
+      </div>
+      <div className="main-right">
+        <img data-aos="fade-left" src={hero} className="hero-img" alt="hero" />
+      </div>
     </div>
-    <div className="main-right">
-        <img
-        data-aos="fade-left" 
-        src={hero} 
-        className="hero-img" 
-        alt="hero"/>
-    </div>
-  </div>
   );
 };
 
